a6e0e6b4e57da9ff7638a9b8562f640d5f43f9b2
MacBoook Pro 15 <macboookpro15@MacBoooks-MacBook-Pro.local>
2026-03-01 15:28:01 +0300

    Auto-install Flutter SDK in Vercel build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Download and install Flutter SDK
+git clone https://github.com/flutter/flutter.git -b 3.19.6
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Build web project
 flutter config --enable-web
+flutter pub get
 flutter build web --release
 
